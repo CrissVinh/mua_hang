6b3fcccb5d08ccc189786ac6059968c7011ba554
CrissVinh <nguyenphanvinh1601@gmail.com>
2026-09-22 23:29:43 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './css/bootstrap.min.css';
+import './css/font-awesome.min.css';
+// import './css/prettyPhoto.css';
+import './css/price-range.css';
+import './css/animate.css';
+import './css/main.css';
+import './css/responsive.css';
+import './css/carticon.css';
+
+import {
+  BrowserRouter,  // bỏ as Router
+  Routes,
+  Route
+}from "react-router-dom";
+
+import Cart from './components/Cart';
+import Home from './components/Home';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      
+        <Routes>
+          <Route path = "/" element = {<App> <Home/> </App>}/>
+          <Route path = "/cart" element = {<App> <Cart/> </App>}/>
+
+        </Routes>
+      
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
