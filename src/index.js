341de1c6f69e1f51a54695c99cc33214f15c3392
CrissVinh <nguyenphanvinh1601@gmail.com>
2026-09-23 18:38:57 +0700

    Update project structure and router

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-import './css/bootstrap.min.css';
-import './css/font-awesome.min.css';
-// import './css/prettyPhoto.css';
-import './css/price-range.css';
-import './css/animate.css';
-import './css/main.css';
-import './css/responsive.css';
-import './css/carticon.css';
-
 import {
   BrowserRouter,  // bỏ as Router
   Routes,
   Route
 }from "react-router-dom";
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 
 import Cart from './components/Cart';
 import Home from './components/Home';
@@ -26,19 +18,16 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      
+      <App>
         <Routes>
-          <Route path = "/" element = {<App> <Home/> </App>}/>
-          <Route path = "/cart" element = {<App> <Cart/> </App>}/>
+          <Route path = "/" element = { <Home/>}/>
+          <Route path = "/cart" element = {<Cart/>}/>
 
         </Routes>
-      
+      </App>
     </BrowserRouter>
     
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
